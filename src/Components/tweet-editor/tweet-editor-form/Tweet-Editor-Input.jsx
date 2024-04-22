f107b723fa34794deb9bc99d7333760e25f7d802
christian birego <93344052+birego@users.noreply.github.com>
2024-04-22 09:56:36 +0200

    ajout des tweet avec axios.post

--- src/Components/tweet-editor/tweet-editor-form/Tweet-Editor-Input.jsx
+++ src/Components/tweet-editor/tweet-editor-form/Tweet-Editor-Input.jsx
@@ -1,13 +1,12 @@
-export default function TweetEditorInput({ register, ref }) {
-    return (
-      <input
-        ref={ref}
-        type="text"
-        name="Post"
-        className="tweet-editor-input"
-        placeholder="What’s happening?"
-        {...register("text", { required: "Veiller ecrire un poste valide" })}
-      />
-    );
-  }
-  
+export default function TweetEditorInput({ register, refs }) {
+  return (
+    <input
+      ref={refs}
+      type="text"
+      name="Post"
+      className="tweet-editor-input"
+      placeholder="What’s happening?"
+      {...register("text", { required: "Veiller ecrire un poste valide" })}
+    />
+  );
+}
--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Tweets from "../Components/Tweets";
 import TweetEditor from "../Components/Tweet-Editor";
 import Header from "../Components/Header";
 import { TweetContext } from "../contexts/tweets";
 
+
 export default function Timeline() {
   const { tweets } = useContext(TweetContext);
   const [tab, setTab] = useState(tweets);
+  useEffect(() => console.log(tab), [tab]);
   return (
     <div>
       <Header />
